e46c5b33fe214febaed5ef398796cd1d14d5bc73
PxGavin <gavin.zhu@pixelmatic.com>
2021-02-25 12:22:43 +0800

    Change build path
    Change build path

--- Assets/Editor/TestBuild.cs
+++ Assets/Editor/TestBuild.cs
@@ -5,12 +5,10 @@ using UnityEngine;
 
 public class TestBuild
 {
-    public static string buildPath = Application.dataPath + "/../build/";
-
     public static void BuildApk()
     {
-        var outdir = $"{buildPath}win/TeamCity_CI_Build";
-        var targetPath = $"{buildPath}win/TeamCity_CI_Build/TeamCity.exe";
+        var outdir = $"c:/bin/win/TeamCity_CI_Build";
+        var targetPath = $"c:/bin/win/TeamCity_CI_Build/TeamCity.exe";
 
         if (!Directory.Exists(outdir)) Directory.CreateDirectory(outdir);
         if (File.Exists(targetPath)) File.Delete(targetPath);
